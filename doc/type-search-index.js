5a587a61cf25c27a1889f60f2ae6c99022f0a0e6
Jerome Habanz <jerome_jero@t-online.de>
2022-06-10 13:19:41 +0200

    added doc

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"VierGewinnt"},{"p":"<Unnamed>","l":"VierGewinntGUI"},{"p":"<Unnamed>","l":"VierGewinntLogic"}];updateSearchResults();
