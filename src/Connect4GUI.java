716ab88fb17e26b8561917a71e227e6654d25298
Jerome Habanz <jerome_jero@t-online.de>
2022-07-01 21:12:30 +0200

    added: gameover as int for winner evaluation

--- src/Connect4GUI.java
+++ src/Connect4GUI.java
@@ -1,13 +1,17 @@
 import processing.core.PApplet;
+import processing.core.PFont;
 import util.Button;
 import util.SelectButton;
 import util.CircleButton;
+
+import javax.swing.*;
 
 public class Connect4GUI extends PApplet {
     Connect4 connect4;
     Button buttonPvC, buttonPvP, buttonUndo, buttonMenu;
     boolean inMenu;
     boolean withAI;
+    PFont font;
 
     public static void main(String[] args) {
         PApplet.runSketch(new String[]{"Connect 4"}, new Connect4GUI());
@@ -21,10 +25,12 @@ public class Connect4GUI extends PApplet {
         background(color(44, 62, 80));
         connect4 = new Connect4Logic();
 
-        buttonPvC = new SelectButton(width / 2,height/ 2,  "PLAYER VS COMPUTER");
-        buttonPvP = new SelectButton(width / 2,height/ 2 + 110,  "PLAYER VS PLAYER");
-        buttonUndo = new CircleButton(width - 50,height - 40, "UNDO");
-        buttonMenu = new CircleButton(width - 150,height - 40, "MENU");
+        font = createFont("files/Oswald-Bold.ttf", 24);
+
+        buttonPvC = new SelectButton(width / 2, height / 2, "PLAYER VS COMPUTER");
+        buttonPvP = new SelectButton(width / 2, height / 2 + 110, "PLAYER VS PLAYER");
+        buttonUndo = new CircleButton(width - 50, height - 40, "UNDO");
+        buttonMenu = new CircleButton(width - 150, height - 40, "MENU");
 
         inMenu = true;
         withAI = false;
@@ -33,7 +39,7 @@ public class Connect4GUI extends PApplet {
     public void draw() {
         int color;
 
-        if(inMenu) {
+        if (inMenu) {
             showMenu();
         } else {
             showBoard();
@@ -44,18 +50,37 @@ public class Connect4GUI extends PApplet {
             if (connect4.isGameOver()) {
                 fill(color(236, 240, 241));
                 textSize(50);
-                text("Game Over", width / 2 - 117, height - 25);
+                String label = "";
+                switch (connect4.intIsGameOver()) {
+                    case 1:
+                        label = "YELLOW WON";
+                        break;
+                    case 2:
+                        label = "RED WON";
+                        break;
+                    case 3:
+                        label = "TIE";
+                        break;
+                    default:
+                        break;
+                }
+                text(label, width / 2 - textWidth(label) / 2, height - textDescent());
             } else {
                 for (int c = 0; c < Connect4.COLUMNS; c++) {
                     if (mouseX > c * 100 && mouseX < (c + 1) * 100) {
                         if (connect4.getPlayer()) {
-                            color = color(231, 76, 60);
+                            color = color(243, 165, 157);
                         } else {
-                            color = color(241, 196, 15);
+                            color = color(248, 225, 135);
                         }
                         fill(color);
-                        ellipse(c * 100 + 50, (6 - connect4.getNextHeight(c)) * 100 + 75, 90, 90);
+                        int height = Connect4.ROWS - (connect4.getNextHeight(c));
+                        System.out.println(height);
+                        if (height < Connect4.ROWS) {
+                            ellipse(c * 100 + 50, height * 100 + 50, 90, 90);
+                        }
                     }
+
                 }
             }
         }
@@ -63,6 +88,12 @@ public class Connect4GUI extends PApplet {
 
     public void showMenu() {
         background(color(44, 62, 80));
+
+        textFont(font);
+        fill(255);
+        textSize(100);
+        text("CONNECT 4", width / 2 - textWidth("CONNECT 4") / 2, height / 4);
+
         buttonPvC.draw(this);
         buttonPvP.draw(this);
     }
@@ -88,36 +119,36 @@ public class Connect4GUI extends PApplet {
                 noStroke();
                 fill(color);
 
-                ellipse(x * 100 + 50, y * 100 + 75, 90, 90);
+                ellipse(x * 100 + 50, y * 100 + 50, 90, 90);
             }
         }
     }
 
     @Override
     public void mouseClicked() {
-        if(inMenu) {
-            if(buttonPvC.mouseOverButton(mouseX, mouseY)) {
+        if (inMenu) {
+            if (buttonPvC.mouseOverButton(mouseX, mouseY)) {
                 inMenu = false;
                 withAI = true;
             }
-            if(buttonPvP.mouseOverButton(mouseX, mouseY)) {
+            if (buttonPvP.mouseOverButton(mouseX, mouseY)) {
                 inMenu = false;
                 withAI = false;
             }
         } else {
-            if(buttonUndo.mouseOverButton(mouseX, mouseY)) {
+            if (buttonUndo.mouseOverButton(mouseX, mouseY)) {
                 try {
                     connect4 = connect4.undoMove();
-                    if(withAI) {
+                    if (withAI) {
                         connect4 = connect4.undoMove();
                     }
-                } catch (Exception e) {
+                } catch (RuntimeException e) {
                     System.out.println(e.getMessage());
                 }
 
             }
 
-            if(buttonMenu.mouseOverButton(mouseX, mouseY)) {
+            if (buttonMenu.mouseOverButton(mouseX, mouseY)) {
                 inMenu = true;
                 connect4 = new Connect4Logic();
             }
@@ -127,10 +158,10 @@ public class Connect4GUI extends PApplet {
                     if (mouseX > i * 100 && mouseX < (i + 1) * 100) {
                         try {
                             connect4 = connect4.play(i);
-                            if(withAI) {
+                            if (withAI) {
                                 connect4 = connect4.play(connect4.bestMove());
                             }
-                        } catch (IllegalArgumentException e) {
+                        } catch (Exception e) {
                             System.out.println(e.getMessage());
                         }
                     }
