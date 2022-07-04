4c407d6938a081c0179b4897f26d350fcb30db73
Jerome Habanz <jerome_jero@t-online.de>
2022-07-04 16:32:53 +0200

    made code easier to understand

--- src/Connect4GUI.java
+++ src/Connect4GUI.java
@@ -4,7 +4,8 @@ import util.Button;
 import util.SelectButton;
 import util.CircleButton;
 
-import javax.swing.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Connect4GUI extends PApplet {
     Connect4 connect4;
@@ -12,6 +13,7 @@ public class Connect4GUI extends PApplet {
     boolean inMenu;
     boolean withAI;
     PFont font;
+    ExecutorService service;
 
     public static void main(String[] args) {
         PApplet.runSketch(new String[]{"Connect 4"}, new Connect4GUI());
@@ -19,6 +21,7 @@ public class Connect4GUI extends PApplet {
 
     public void settings() {
         size(Connect4.COLUMNS * 100, (Connect4.ROWS + 1) * 100);
+        service = Executors.newSingleThreadExecutor();
     }
 
     public void setup() {
@@ -37,72 +40,59 @@ public class Connect4GUI extends PApplet {
     }
 
     public void draw() {
-        int color;
-
         if (inMenu) {
-            showMenu();
+            drawMenu();
         } else {
-            showBoard();
-
-            buttonUndo.draw(this);
-            buttonMenu.draw(this);
-
-            if (connect4.isGameOver()) {
-                fill(color(236, 240, 241));
-                textSize(50);
-                String label = "";
-                switch (connect4.intIsGameOver()) {
-                    case 1:
-                        label = "YELLOW WON";
-                        break;
-                    case 2:
-                        label = "RED WON";
-                        break;
-                    case 3:
-                        label = "TIE";
-                        break;
-                    default:
-                        break;
-                }
-                text(label, width / 2 - textWidth(label) / 2, height - textDescent());
-            } else {
-                for (int c = 0; c < Connect4.COLUMNS; c++) {
-                    if (mouseX > c * 100 && mouseX < (c + 1) * 100) {
-                        if (connect4.getPlayer()) {
-                            color = color(243, 165, 157);
-                        } else {
-                            color = color(248, 225, 135);
-                        }
-                        fill(color);
-                        int height = Connect4.ROWS - (connect4.getNextHeight(c));
-                        if (height < Connect4.ROWS) {
-                            ellipse(c * 100 + 50, height * 100 + 50, 90, 90);
-                        }
-                    }
-                }
-            }
+            drawBoard();
         }
     }
 
-    public void showMenu() {
+    public void drawMenu() {
         background(color(44, 62, 80));
+
 
         textFont(font);
         fill(255);
         textSize(100);
         text("CONNECT 4", width / 2 - textWidth("CONNECT 4") / 2, height / 4);
 
+
         buttonPvC.draw(this);
         buttonPvP.draw(this);
     }
 
-    public void showBoard() {
+    public void drawBoard() {
         background(color(44, 62, 80));
         int color;
 
-        for (int y = 0; y < Connect4.ROWS; y++) {
-            for (int x = 0; x < Connect4.COLUMNS; x++) {
-                switch (connect4.getBoard(x, y)) {
+
+        buttonUndo.draw(this);
+        buttonMenu.draw(this);
+
+        //Game Over
+        if (connect4.isGameOver()) {
+            fill(color(236, 240, 241));
+            textSize(50);
+            String label = "";
+            switch (connect4.intIsGameOver()) {
+                case 1:
+                    label = "YELLOW WON";
+                    break;
+                case 2:
+                    label = "RED WON";
+                    break;
+                case 3:
+                    label = "TIE";
+                    break;
+            }
+            text(label, width / 2 - textWidth(label) / 2, height - textDescent());
+        }
+
+
+        //Draw the Board
+        for (int c = 0; c < Connect4.COLUMNS; c++) {
+            for (int r = 0; r < Connect4.ROWS; r++) {
+                switch (connect4.getBoard(c, r)) {
                     case 1:
                         color = color(231, 76, 60);
                         break;
@@ -117,54 +107,64 @@ public class Connect4GUI extends PApplet {
                 noStroke();
                 fill(color);
 
-                ellipse(x * 100 + 50, y * 100 + 50, 90, 90);
+                ellipse(c * 100 + 50, r * 100 + 50, 90, 90);
             }
+        }
+
+
+        //Draw the current player on mouse X
+        color = connect4.getPlayer() ? color(243, 165, 157) : color(248, 225, 135);
+
+        fill(color);
+        int height = Connect4.ROWS - (connect4.getNextHeight(mouseX / 100));
+
+        if (height < Connect4.ROWS) {
+            ellipse(mouseX / 100 * 100 + 50, height * 100 + 50, 90, 90);
         }
     }
 
-    @Override
     public void mouseClicked() {
-        if (inMenu) {
-            if (buttonPvC.mouseOverButton(mouseX, mouseY)) {
-                inMenu = false;
-                withAI = true;
-            }
-            if (buttonPvP.mouseOverButton(mouseX, mouseY)) {
-                inMenu = false;
-                withAI = false;
-            }
-        } else {
-            if (buttonUndo.mouseOverButton(mouseX, mouseY)) {
-                try {
+        if (!service.isTerminated()) {
+            service.submit(() -> {
+                mouseOverAnyButton();
+            });
+        }
+    }
+
+    public void mouseOverAnyButton() {
+        try {
+            if (inMenu) {
+                if (buttonPvC.mouseOver(mouseX, mouseY)) {
+                    inMenu = false;
+                    withAI = true;
+                }
+                if (buttonPvP.mouseOver(mouseX, mouseY)) {
+                    inMenu = false;
+                    withAI = false;
+                }
+            } else {
+                if (buttonUndo.mouseOver(mouseX, mouseY)) {
                     connect4 = connect4.undoMove();
                     if (withAI) {
                         connect4 = connect4.undoMove();
                     }
-                } catch (RuntimeException e) {
-                    System.out.println(e.getMessage());
+
                 }
 
-            }
+                if (buttonMenu.mouseOver(mouseX, mouseY)) {
+                    inMenu = true;
+                    connect4 = new Connect4Logic();
+                }
 
-            if (buttonMenu.mouseOverButton(mouseX, mouseY)) {
-                inMenu = true;
-                connect4 = new Connect4Logic();
-            }
-
-            if (!connect4.isGameOver() && mouseY < height - 100) {
-                for (int i = 0; i < Connect4.COLUMNS; i++) {
-                    if (mouseX > i * 100 && mouseX < (i + 1) * 100) {
-                        try {
-                            connect4 = connect4.play(i);
-                            if (withAI) {
-                                connect4 = connect4.play(connect4.bestMove());
-                            }
-                        } catch (Exception e) {
-                            System.out.println(e.getMessage());
-                        }
+                if (!connect4.isGameOver() && mouseY < height - 100) {
+                    connect4 = connect4.play(mouseX / 100);
+                    if (withAI) {
+                        connect4 = connect4.play(connect4.bestMove());
                     }
                 }
             }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 }
