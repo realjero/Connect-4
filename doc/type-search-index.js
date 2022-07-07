da961e6442aeba8a2a0b603a5e0fcc262cbfb303
Jerome Habanz <jerome_jero@t-online.de>
2022-07-07 15:08:17 +0200

    generated new doc

--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"VierGewinnt"},{"p":"<Unnamed>","l":"VierGewinntGUI"},{"p":"<Unnamed>","l":"VierGewinntLogic"}];updateSearchResults();
