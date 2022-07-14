ea53d763f748b5caf0c267f911b736e97ec8cdbb
Jerome Habanz <jerome_jero@t-online.de>
2022-07-14 23:18:11 +0200

    minor change

---
+++ src/Connect4.java
@@ -0,0 +1,20 @@
+interface Connect4 {
+    int COLUMNS = 7;
+    int ROWS = 6;
+
+    Connect4Logic play(int column);
+
+    Connect4Logic undoMove();
+
+    int bestMove();
+
+    int intIsGameOver();
+
+    boolean isGameOver();
+
+    int getBoard(int row, int column);
+
+    boolean getPlayer();
+
+    int getNextHeight(int column);
+}
